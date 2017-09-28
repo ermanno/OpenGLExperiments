7479622accb6d9caefa0b3366579a4d741199bc0
ermanno <moser.ermanno@gmail.com>
2017-09-28 23:38:49 +0200

    Add Makefile which can correctly compile the program.

--- Makefile
+++
@@ -1,7 +0,0 @@
-# Note: the following line works, I just need to understand how to
-# create a Makefile that includes glad.c in the project:
-# g++	 -I/home/ermanno/libraries/glad/include		HelloWorldOpenGL.cpp /home/ermanno/libraries/glad/src/glad.c	-lGLEW -lglfw3 -lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -pthread -ldl -o HelloWorldOpenGL
-
-CPPFLAGS=-I/home/ermanno/libraries/glad/include
-LDLIBS=-lGLEW -lglfw3 -lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -pthread -ldl
-all: HelloWorldOpenGL
---
+++ WindowCreation/HelloWindow.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    // Initialize GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a window and make its context the current context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Tell GLAD which functions it should use to find the addresses
+    // of the OpenGL functions
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Set the viewport and make it handled window resizing
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up resources
+    glfwTerminate();
+    return 0;
+}
